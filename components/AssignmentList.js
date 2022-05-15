16a6c85fa9732310f81ee06c92f92505cf6ef8f0
Trin4317 <eg.artourbabaev@gmail.com>
2022-05-15 21:43:13 +0900

    update AssignmentList component to show available tags

--- components/AssignmentList.js
+++ components/AssignmentList.js
@@ -7,9 +7,16 @@ export default {
 
 	template: `
 		<section v-show="assignments.length">
-			<h2 class="font-bold mb-2">{{ title }}</h2>
+			<h2 class="font-bold mb-2">
+				{{ title }}
+				<span>({{ assignments.length }})</span>
+			</h2>
 
-			<ul class="border border-gray-600 divide-y divide-gray-600">
+			<div class="flex gap-2">
+				<button v-for="tag in tags" class="border rounded px-1 py-px text-xs">{{ tag }}</button>
+			</div>
+
+			<ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
 				<assignment
 					v-for="assignment in assignments"
 					:key="assignment.id"
@@ -22,5 +29,11 @@ export default {
 	props: {
 		assignments: Array,
 		title: String
+	},
+
+	computed: {
+		tags() {
+			return new Set(this.assignments.map(a => a.tag));
+		}
 	}
 }
